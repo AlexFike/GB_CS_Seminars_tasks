76fa07537c5d12c683a4c8cc4dff2534ae89ad8d
Alex <alexfike2016@gmail.com>
2022-11-29 16:09:39 +0500

    task_0

--- task_0/Program.cs
+++ task_0/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 Console.Write("input number: ");
 
-int number = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine($"Result = {number * number}");
 
