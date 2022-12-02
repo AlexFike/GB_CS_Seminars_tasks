4f52bb28dabb9d610cbe16f7b578799f437eae7c
Alex <alexfike2016@gmail.com>
2022-12-02 17:45:44 +0500

    task_11 delete digit

---
+++ 11_delete_second_dgit_of_number/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int DelTwoDigitNum(int number)
+{
+
+    int number1 = number / 100,
+        number3 = number%10;
+
+    int result = number1 * 10 + number3;
+
+    return result;  
+}
+
+int numberTest = new Random().Next(100, 1000);
+Console.WriteLine($"Delet two digit for {numberTest}. Result = {DelTwoDigitNum(numberTest)}");
