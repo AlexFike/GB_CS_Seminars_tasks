b1b0546406d3ee70d2125f0861755130edc0cdd1
Alex <alexfike2016@gmail.com>
2022-12-02 15:10:06 +0500

    task_10 second digit of number

---
+++ 10_second_dgit_of_number/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Clear();
+
+int SecondDigit(int number)
+{
+
+    int result = (number%100)/10;
+
+    return result;
+}
+
+int rndNumber = new Random().Next(100,1000);
+
+Console.WriteLine($"the second digit of the number = {SecondDigit(rndNumber)}");
+
