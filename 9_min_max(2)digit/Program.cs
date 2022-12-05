a0ebfb1d98783a97c3a563f394d793535cc0227e
Alex <alexfike2016@gmail.com>
2022-12-05 14:04:24 +0500

    task_9 max digit

---
+++ 9_min_max(2)digit/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное
+// число из отрезка [10, 99] и показывает наибольшую
+// цифру числа.
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8 
+
+
+int numberRnd = new Random().Next(10,100);
+
+int result = Math.Max(numberRnd / 10, numberRnd % 10);
+
+Console.WriteLine($"Maximum digit = {result}");
