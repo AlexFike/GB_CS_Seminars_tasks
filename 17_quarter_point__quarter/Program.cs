e92db1e87e4711b606d15e673f129c35ff2f8ecc
Alex <alexfike2016@gmail.com>
2022-12-06 16:08:27 +0500

    task_17

---
+++ 17_quarter_point__quarter/Program.cs
@@ -0,0 +1,26 @@
+﻿int SearchQuarter(int x, int y)
+{
+    switch ((x, y))
+    {
+        case ( > 0, > 0):
+            return 1;
+        case ( <0, > 0):
+            return 2;
+        case ( < 0, < 0):
+            return 3;
+        case ( > 0, < 0):
+            return 4;
+        default:
+            return 0;
+    }
+}
+
+
+Console.Write("Input point X: ");
+int numberX = int.Parse(Console.ReadLine()!);
+
+Console.Write("Input point Y: ");
+int numberY = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Point in quarter => {SearchQuarter(numberX,numberY)}");
+
