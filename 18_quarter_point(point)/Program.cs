69fa96c6fb172e65125703f38e3a6d8ebf319bcb
Alex <alexfike2016@gmail.com>
2022-12-06 16:19:45 +0500

    task_18

---
+++ 18_quarter_point(point)/Program.cs
@@ -0,0 +1,26 @@
+﻿void SearthPoint(int quarter)
+{
+    switch (quarter)
+    {
+        case 1:
+            Console.WriteLine("Point in x > 0 and y > 0");
+            break;
+        case 2:
+            Console.WriteLine("Point in x < 0 and y > 0");
+            break;
+        case 3:
+            Console.WriteLine("Point in x < 0 and y < 0");
+            break;
+        case 4:
+            Console.WriteLine("Point in x > 0 and y < 0");
+            break;
+        default:
+            Console.WriteLine("No valid is quarter");
+            break;
+    }
+}
+
+Console.Write("Input quarter: ");
+int numberQ = int.Parse(Console.ReadLine()!);
+
+SearthPoint(numberQ);
