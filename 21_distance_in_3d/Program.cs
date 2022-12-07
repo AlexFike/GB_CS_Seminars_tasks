ffc07b4333ee306d80ee7d31338755d7e7e0cc1c
Alex <alexfike2016@gmail.com>
2022-12-07 17:10:46 +0500

    task_21

--- 21_distance_in_3d/Program.cs
+++ 21_distance_in_3d/Program.cs
@@ -3,3 +3,14 @@
 
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double distansPoints(int xA, int yA, int zA, int xB, int yB, int zB)
+{
+    double result = Math.Sqrt((Math.Pow((xB - xA), 2)) +
+                              (Math.Pow((yB - yA), 2)) +
+                              (Math.Pow((zB - zA), 2)));
+
+    return result;
+}
+
+Console.WriteLine(string.Format("{0:F3}", distansPoints(7, -5, 0, 1, -1, 9)));
