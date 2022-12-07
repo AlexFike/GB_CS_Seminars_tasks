31863a0d0b4a3010a1eb0441f448bfac5642f4bd
Alex <alexfike2016@gmail.com>
2022-12-07 16:05:03 +0500

    task_19 palindrome

--- 19_palindrome/Program.cs
+++ 19_palindrome/Program.cs
@@ -4,3 +4,31 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.Clear();
+
+
+
+bool palindrome(int num)
+{
+    int mlt = Convert.ToInt32(Math.Floor(Math.Log10(num)));
+    int numCheck = num, stop = mlt / 2;
+
+    while (stop >= 0)
+    {
+        if (numCheck % 10 == Convert.ToInt32(numCheck / Math.Pow(10, mlt)))
+        {
+            numCheck = Convert.ToInt32(numCheck % Math.Pow(10, mlt)) / 10;
+            stop--;
+            mlt -= 2;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+Console.WriteLine(palindrome(14212));
+
