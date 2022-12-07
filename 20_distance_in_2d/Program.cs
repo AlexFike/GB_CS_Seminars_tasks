97790cc580e994b7876d0870073b2982b39ab0f5
Alex <alexfike2016@gmail.com>
2022-12-07 16:22:46 +0500

    task_20

--- 20_distance_in_2d/Program.cs
+++ 20_distance_in_2d/Program.cs
@@ -4,3 +4,12 @@
 // A (3,6); B (2,1) -> 5,09 
 // A (7,-5); B (1,-1) -> 7,21
 
+
+double distansPoints(int xA, int yA, int xB, int yB)
+{
+    double result = Math.Sqrt((Math.Pow((xB - xA), 2)) + (Math.Pow((yB - yA), 2)));
+
+    return result;
+}
+
+Console.WriteLine(string.Format("{0:F3}", distansPoints(3, 6, 2, 1)));
