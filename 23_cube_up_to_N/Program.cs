249d204a81dad39c0dc7dcd5e53190f19e24fd81
Alex <alexfike2016@gmail.com>
2022-12-07 17:33:24 +0500

    task_23 recursion

--- 23_cube_up_to_N/Program.cs
+++ 23_cube_up_to_N/Program.cs
@@ -3,3 +3,18 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+void RecCube(int n, int i = 1)
+{
+    if (n == i)
+    {
+        Console.Write(i * i * i);
+    }
+    else
+    {
+        Console.Write($"{i * i * i} ");
+        RecCube(n, i + 1);
+    }
+}
+
+RecCube(5);
