29f2bbd719a8b39db0d9b5e0a7f371123ef09e1f
Alex <alexfike2016@gmail.com>
2022-12-12 13:03:58 +0500

    task 27 rec

---
+++ 27_sum_digit/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает
+// на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumDigit(int number)
+{
+    if (number == 0) return 0;
+    else return number % 10 + SumDigit(number / 10);
+}
+
+Console.WriteLine(SumDigit(5555));
