7c244536c5abd5669f437bef353f5b45566fcb82
Alex <alexfike2016@gmail.com>
2022-12-14 14:15:17 +0500

    task 36

---
+++ 36_sum_odd_pos_array/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int start, int end)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(start, end);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+int SumOddPosArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2) sum += array[i];
+    return sum;
+}
+
+
+int[] testArray = CreateArrayRndInt(10, 1, 100);
+
+PrintArray(testArray);
+Console.Write($" => {SumOddPosArray(testArray)}");
