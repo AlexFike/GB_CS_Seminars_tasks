277a043ded7f4cd344aa56e05506cb545bbd31d1
Alex <alexfike2016@gmail.com>
2022-12-16 17:19:48 +0500

    task 41

---
+++ 41_numbers_positive_count/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int NumbersPositivCount(string[] collection)
+{
+    int count = 0;
+    foreach (var item in collection)
+    {
+        if (int.Parse(item) > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Input number by space: ");
+string[] array = Console.ReadLine()!.Split();
+
+Console.WriteLine(NumbersPositivCount(array));
