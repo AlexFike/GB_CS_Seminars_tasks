d53cc801776da02ff243dc012c5455f0465a727f
Alex <alexfike2016@gmail.com>
2022-12-21 14:25:30 +0500

    tasks 47, 50, 52 matrix!?

---
+++ 47_matrix_double/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GetArray(int m, int n, int start, int end)
+{
+    double[,] array = new double[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int tm = rnd.Next(0, 2);
+            if (tm == 0) array[i, j] = rnd.Next(start, end);
+            else array[i, j] = Math.Round((rnd.Next(start, end) + rnd.NextDouble()), 1);
+
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Input rows array: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Input columns array: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+Console.Write("Input start rnd: ");
+int start = int.Parse(Console.ReadLine()!);
+
+Console.Write("Input end rnd: ");
+int end = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows, columns, start, end);
+
+PrintArray(array);
+
+
+
---
+++ 50_search_position/Program.cs
@@ -0,0 +1,75 @@
+﻿// Напишите программу, которая на вход принимает 
+// позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// [1,7] -> такого числа в массиве нет ([1,7] это позиция по индексам, 1- строка и 7 -столбец
+
+
+int[,] GetArray(int m, int n, int start, int end)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(start, end);
+        }
+    }
+    return array;
+}
+
+void PrinArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = GetArray(4, 4, 1, 100);
+
+void SearchElementInMatrix(int[,] array)
+{
+    int count = 5;
+    while (true)
+    {
+        Console.WriteLine("Input position of element ↓");
+        Console.Write("rows => ");
+        int rows = int.Parse(Console.ReadLine()!);
+        Console.Write("columns => ");
+        int columns = int.Parse(Console.ReadLine()!);
+        try
+        {
+            Console.Write($"{array[rows, columns]}");
+            Console.Write($" <= element in position with indexes [{rows}, {columns}]");
+            break;
+        }
+        catch
+        {
+            Console.WriteLine("There is no such position or you have entered incorrect data!");
+            count--;
+            if (count == 0)
+            {
+                Console.WriteLine("0 attempts! try it another time...");
+                break;
+            }
+            else if (count == 1) Console.WriteLine("More 1 attempt!");
+            else Console.WriteLine($"More {count} attempts");
+        }
+
+
+    }
+}
+
+PrinArray(array);
+SearchElementInMatrix(array);
+
