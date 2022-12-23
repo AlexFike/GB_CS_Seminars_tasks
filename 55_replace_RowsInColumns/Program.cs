572974eb14530a34a14ffdef5e9d242f3ab13e81
Alex <alexfike2016@gmail.com>
2022-12-23 15:31:13 +0500

    tasks seminar: 55

---
+++ 55_replace_RowsInColumns/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+// которая заменяет строки на столбцы. В случае, если это невозможно, 
+// программа должна вывести сообщение для пользователя.
+
+
+int[,] ReplaceRowsInColumns(int[,] array)
+{
+    int[,] newArray = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            newArray[j, i] = array[i, j];
+        }
+    }
+    return newArray;
+}
+
+
+void CheckValidArrayReplace(int[,] array)
+{
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("Array not valid!");
+    }
+    else
+    {
+        PrintArray(ReplaceRowsInColumns(array));
+    }
+
+}
+
+
+int[,] GetArray(int m, int n, int start, int end)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(start, end);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] matrix = GetArray(5, 5, 1, 10);
+PrintArray(matrix);
+Console.WriteLine();
+CheckValidArrayReplace(matrix);
+Console.WriteLine();
+PrintArray(matrix);
